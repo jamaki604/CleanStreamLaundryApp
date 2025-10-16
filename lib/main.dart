836aac4ee4047c722ac0244184a951038d804f68
James <jarjrjarsr@gmail.com>
2025-10-16 14:02:56 -0400

    change favicon as well as add env implementation for supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:clean_stream_laundry_app/Pages/RootApp.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async{
+  await dotenv.load(fileName: '.env');
+  print('${dotenv.env['SUPABASE_URL']}');
   WidgetsFlutterBinding.ensureInitialized();
-  /*
+
   await Supabase.initialize(
-    url: 'SUPABASE_URL',
-    amonKey: 'AMON_KEY'
+    url: '${dotenv.env['SUPABASE_URL']}',
+    anonKey: '${dotenv.env['ANON_KEY']}'
   );
-  */
+
 
   runApp(const RootApp());
 }
