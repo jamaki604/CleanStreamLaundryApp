3205a47b956d17ce19f936d6d014e82d36efa688
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2025-10-22 16:12:11 -0400

    Fixed .env file

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async{
-  await dotenv.load(fileName: 'lib/assets/.env');
+  await dotenv.load(fileName: '.env');
   print('${dotenv.env['SUPABASE_URL']}');
   WidgetsFlutterBinding.ensureInitialized();
 
