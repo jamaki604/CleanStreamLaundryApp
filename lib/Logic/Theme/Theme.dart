a11e639a3ad27e1d94158921cbb14e9303c5d943
JMiller921387 <tyfsak61@outlook.com>
2025-10-30 19:22:59 -0400

    Added Dark Mode functionality
    Can now change between light mode and dark mode

---
+++ lib/Logic/Theme/Theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    brightness: Brightness.light,
+    background: Colors.white,
+    tertiary: Colors.indigo[900],
+  )
+);
+
+ThemeData darkMode = ThemeData(
+  brightness: Brightness.dark,
+    colorScheme: ColorScheme.light(
+      brightness: Brightness.dark,
+      background: Colors.grey.shade900,
+      tertiary: Colors.deepPurple,
+    )
+);
+
+extension ModeChangerText on ColorScheme {
+  String get modeChangerText {
+    return brightness == Brightness.dark
+        ? "Change to Light Mode"
+        : "Change to Dark Mode";
+  }
+}
--- lib/Pages/RootApp.dart
+++ lib/Pages/RootApp.dart
@@ -5,12 +5,33 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
 
 class RootApp extends StatefulWidget {
-  const RootApp({super.key});
+  final ThemeData theme;
+  const RootApp({super.key, required this.theme});
 
   @override
   State<RootApp> createState() => _RootAppState();
 }
 
+class _RootAppState extends State<RootApp> {
+  late final AuthSystem _authenticator;
+
+  @override
+  void initState() {
+    super.initState();
+    _authenticator = Authenticator(Supabase.instance.client);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Clean Stream Laundry Solutions',
+      theme: widget.theme,
+      routerConfig: createRouter(_authenticator),
+    );
+  }
+}
+/*
 class _RootAppState extends State<RootApp> {
   final AuthSystem _authenticator = Authenticator(Supabase.instance.client);
   @override
@@ -26,3 +47,4 @@ class _RootAppState extends State<RootApp> {
     );
   }
 }
+*/
