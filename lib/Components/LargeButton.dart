737290caf29ed75b7c1a21b3bafc5524caba0095
Jason Yoder <jason.yoder2@bsu.edu>
2025-11-05 17:11:12 -0500

    Makes Card element on wallet page a component and removes unused imports

--- lib/Components/LargeButton.dart
+++ lib/Components/LargeButton.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class LargeButton extends StatelessWidget {
   final String headLineText;
