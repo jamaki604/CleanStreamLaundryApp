06d5b9692ef8237678fe084e2dda396662eacf60
James <jarjrjarsr@gmail.com>
2025-11-09 08:27:32 -0500

    Created monthly transaction history

--- lib/Logic/Transaction/transaction_parser.dart
+++ lib/Logic/Transaction/transaction_parser.dart
@@ -10,18 +10,61 @@ class TransactionParser {
     final formattedAmount = '\$${amount.toStringAsFixed(2)}';
     int currentMonth = DateTime.now().month;
 
-    if(DateFormat('M').format(createdAt) != currentMonth.toString() ){
+    if (DateFormat('M').format(createdAt) != currentMonth.toString()) {
       return "";
     }
 
-    if(description == "Loyalty Card"){
+    if (description == "Loyalty Card") {
       return '$formattedAmount added to $description on $formattedDate';
-    }else{
+    } else {
       return '$formattedAmount used on $description on $formattedDate';
     }
   }
 
-  static List<String> formatTransactionsList(Iterable<Map<String, dynamic>> data) {
+  static List<String> formatTransactionsList(
+    Iterable<Map<String, dynamic>> data,
+  ) {
     return data.map((transaction) => formatTransaction(transaction)).toList();
+  }
+
+  static Map<String, Map<String, double>> getMonthlySums(
+    List<Map<String, dynamic>> transactions,
+  ) {
+    final now = DateTime.now();
+    final result = <String, Map<String, double>>{};
+    int currentMonth = DateTime.now().month;
+
+    for (int i = 1; i <= 12; i++) {
+      final monthDate = DateTime(now.year, now.month - i, 1);
+      final monthKey = DateFormat('MMM yyyy').format(monthDate);
+      result[monthKey] = {'washer': 0.0, 'dryer': 0.0, 'loyaltyCard': 0.0};
+    }
+
+    final cutoffDate = DateTime(now.year-1, now.month, 1);
+    for (final transaction in transactions) {
+
+      final createdAt = DateTime.parse(transaction['created_at'] as String);
+
+      if (createdAt.isBefore(cutoffDate)) {
+        break;
+      }
+
+      if(DateFormat('M').format(createdAt) == currentMonth.toString()){
+        continue;
+      }
+
+      final amount = (transaction['amount'] as num).toDouble();
+      final description = (transaction['description'] as String).toLowerCase();
+      final monthKey = DateFormat('MMM yyyy').format(createdAt);
+
+      if (description.contains('washer')) {
+        result[monthKey]!['washer'] = result[monthKey]!['washer']! + amount;
+      } else if (description.contains('dryer')) {
+        result[monthKey]!['dryer'] = result[monthKey]!['dryer']! + amount;
+      } else if (description == 'loyalty card') {
+        result[monthKey]!['loyaltyCard'] = result[monthKey]!['loyaltyCard']! + amount;
+      }
+    }
+    return result;
   }
 }
