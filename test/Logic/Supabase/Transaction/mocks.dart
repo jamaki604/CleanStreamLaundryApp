45f0696139175c12aeb7c266f828608c36225243
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2025-11-09 20:24:37 -0500

    Added tests for transaction handler

--- test/Logic/Supabase/Transaction/mocks.dart
+++ test/Logic/Supabase/Transaction/mocks.dart
@@ -4,56 +4,41 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseMock extends Mock implements SupabaseClient {}
 class QueryBuilderMock extends Mock implements SupabaseQueryBuilder {}
+class GoTrueMock extends Mock implements GoTrueClient {}
 
 class FakeFilterBuilder extends Fake implements PostgrestFilterBuilder<PostgrestList> {
-  final Map<String, dynamic> fakeData;
+  final List<Map<String, dynamic>> fakeData;
 
   FakeFilterBuilder(this.fakeData);
 
   @override
-  PostgrestTransformBuilder<Map<String, dynamic>> single() {
-    return FakeTransformBuilder(fakeData);
+  Future<U> then<U>(FutureOr<U> Function(PostgrestList) onValue, {Function? onError,}) {
+    try {
+
+      final result = onValue(fakeData);
+      return Future.value(result);
+    } catch (e) {
+      if (onError != null) {
+        onError(e);
+        return Future.error(e);
+      }
+      return Future.error(e);
+    }
   }
 
   @override
-  Future<U> then<U>(FutureOr<U> Function(PostgrestList) onValue, {Function? onError,}) async {
-    try {
-      final result = onValue([fakeData] as PostgrestList);
-      return Future.value(result);
-    } catch (e) {
-      if (onError != null) onError(e);
-      return Future.error(e);
-    }
+  Future<PostgrestList> catchError(Function onError, {bool Function(Object)? test}) {
+    return Future.value(fakeData);
   }
 
   @override
   PostgrestFilterBuilder<PostgrestList> eq(String column, dynamic value) {
     return this;
   }
-}
-
-class FakeTransformBuilder extends Fake implements PostgrestTransformBuilder<Map<String, dynamic>> {
-  final Map<String, dynamic> fakeData;
-  FakeTransformBuilder(this.fakeData);
 
   @override
-  Future<U> then<U>(FutureOr<U> Function(Map<String, dynamic>) onValue, {Function? onError,}) async {
-    try {
-      final result = onValue(fakeData);
-      return Future.value(result);
-    } catch (e) {
-      if (onError != null) onError(e);
-      return Future.error(e);
-    }
+  PostgrestTransformBuilder<List<Map<String, dynamic>>> order(String column, {bool ascending = true, bool nullsFirst = false, String? referencedTable}) {
+    return this;
   }
 
-  @override
-  Map<String, dynamic> eq(String column, dynamic value) {
-    return fakeData;
-  }
-
-  @override
-  PostgrestTransformBuilder<PostgrestMap> single() {
-    return this as PostgrestTransformBuilder<PostgrestMap>;
-  }
 }
--- test/Logic/Supabase/Transaction/transaction_handler_test.dart
+++ test/Logic/Supabase/Transaction/transaction_handler_test.dart
@@ -1,30 +1,51 @@
+import 'package:clean_stream_laundry_app/Logic/Supabase/Transaction/transaction_handler.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:clean_stream_laundry_app/Logic/Supabase/Machine/machine_handler.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'mocks.dart';
 
 void main() {
   late SupabaseMock supabaseMock;
   late QueryBuilderMock queryBuilderMock;
   late FakeFilterBuilder fakeFilterBuilder;
-  late MachineHandler machineHandler;
+  late TransactionHandler transactionHandler;
+  late GoTrueMock supabaseAuth;
 
   setUp(() {
     supabaseMock = SupabaseMock();
     queryBuilderMock = QueryBuilderMock();
-    fakeFilterBuilder = FakeFilterBuilder(5);
-    machineHandler = MachineHandler(client: supabaseMock);
+    fakeFilterBuilder = FakeFilterBuilder([{"amount": 2.75, "description": "Machine, created_at: 2025-11-02T16:24:51.685419+00:00"}, {"amount": 2.75, "description": "Machine, created_at: 2025-10-28T15:13:24.87605+00:00"}, {"amount": 2.75, "description": "Machine, created_at: 2025-10-28T14:27:54.429939+00:00"}, {"amount": 2.75, "description": "Machine, created_at: 2025-10-28T14:26:21.662999+00:00"}, {"amount": 2.75, "description": "Machine, created_at: 2025-10-27T18:06:40.987278+00:00"}, {"amount": 2.75, "description": "Machine, created_at: 2025-10-27T00:17:18.01511+00:00"}]);
+    transactionHandler = TransactionHandler(client: supabaseMock);
+    supabaseAuth = GoTrueMock();
 
-    when(() => supabaseMock.from('Machines')).thenAnswer((
-        _) => queryBuilderMock);
-    when(() => queryBuilderMock.select(any())).thenAnswer((
-        _) => fakeFilterBuilder);
+    when(() => supabaseMock.from('transactions')).thenAnswer((_) => queryBuilderMock);
+    when(() => queryBuilderMock.select(any())).thenAnswer((_) => fakeFilterBuilder);
+    when(() => supabaseMock.auth).thenReturn(supabaseAuth);
+
+    final mockUser = User(
+      id: '11111111-1111-1111-1111-111111111111',
+      aud: 'authenticated',
+      role: 'authenticated',
+      email: 'testemail@test.com',
+      emailConfirmedAt: null,
+      phone: '',
+      lastSignInAt: '',
+      appMetadata: {},
+      userMetadata: {},
+      identities: [],
+      createdAt: '',
+      updatedAt: '',
+    );
+
+    when(() => supabaseAuth.currentUser).thenReturn(mockUser);
+
   });
 
   group('Transaction Tests',(){
 
-    test("Get transaction history data",(){
-
+    test("Get transaction history data",() async {
+      final result = await transactionHandler.getTransactionsForUser();
+      expect(result.length, 6);
     });
 
   });
