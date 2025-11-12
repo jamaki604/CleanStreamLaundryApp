331176fd29208480a0ee99214a224a382e3c1f7d
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2025-11-12 16:18:54 -0500

    Added unit test for TransactionParser

--- test/Logic/Parser/transaction_parser_test.dart
+++ test/Logic/Parser/transaction_parser_test.dart
@@ -12,10 +12,15 @@ void main(){
       expect(result, "\$2.75 used on Dryer on Nov 12, 2025");
     });
 
-    test("Test that transactions are parsed correctly if descrpition is Loyalty Card",(){
+    test("Test that transactions are parsed correctly if description is Loyalty Card",(){
 
       final result = TransactionParser.formatTransaction(({"amount": 2.75, "description": "Loyalty Card", "created_at": "2025-11-12T19:23:24.781326+00:00"}));
       expect(result, "\$2.75 added to Loyalty Card on Nov 12, 2025");
+    });
+
+    test("Test that it can format a list of transactions",(){
+      final result = TransactionParser.formatTransactionsList([{"amount": 2.75, "description": "Dryer", "created_at": "2025-11-12T19:23:24.781326+00:00"},{"amount": 4.75, "description": "Washer", "created_at": "2025-11-12T19:23:24.781326+00:00"}]);
+      expect(result[0], "\$2.75 used on Dryer on Nov 12, 2025");
     });
 
   });
