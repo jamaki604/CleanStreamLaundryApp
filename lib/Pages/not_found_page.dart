a67c12099b2c4ae7bccb764d2c9533440dee7d80
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2025-11-13 14:49:22 -0500

    Updated error page

--- lib/Pages/not_found_page.dart
+++ lib/Pages/not_found_page.dart
@@ -1,6 +1,9 @@
 import 'package:clean_stream_laundry_app/Components/base_page.dart';
+import 'package:clean_stream_laundry_app/Logic/Enums/authentication_response_enum.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import '../Logic/Services/auth_service.dart';
 import '../Logic/Theme/theme.dart';
 
 class NotFoundScreen extends StatelessWidget {
@@ -8,24 +11,34 @@ class NotFoundScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BasePage(
-      body: Center(
+
+    final authService = GetIt.instance<AuthService>();
+
+    return Container(
+      color: Colors.white,
+      child: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             const Icon(Icons.error_outline, color: Colors.redAccent, size: 80),
             const SizedBox(height: 20),
-            Text(
+            const Text(
               '404 - Page Not Found',
               style: TextStyle(
                 fontSize: 24,
-                color: Theme.of(context).colorScheme.fontPrimary,
+                color: Colors.blue,
               ),
             ),
             const SizedBox(height: 16),
             ElevatedButton(
-              onPressed: () => context.go('/scanner'),
-              child: const Text('Go Home'),
+              onPressed: () async {
+                if (await authService.isLoggedIn() == AuthenticationResponses.success){
+                  context.go("/homePage");
+                }else{
+                  context.go("/login");
+                }
+              },
+              child: const Text("Go to Home"),
             ),
           ],
         ),
