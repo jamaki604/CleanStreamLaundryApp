627356c01e933f18061badba84da4592642ea6fa
Jason Yoder <jason.yoder2@bsu.edu>
2025-12-03 13:08:20 -0500

    Fix import paths in not_found_page.dart

--- lib/pages/not_found_page.dart
+++ lib/pages/not_found_page.dart
@@ -2,7 +2,7 @@ import 'package:clean_stream_laundry_app/logic/enums/authentication_response_enu
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
-import 'package:clean_stream_laundry_app/Logic/Services/auth_service.dart';
+import 'package:clean_stream_laundry_app/logic/services/auth_service.dart';
 
 class NotFoundScreen extends StatelessWidget {
   const NotFoundScreen({super.key});
