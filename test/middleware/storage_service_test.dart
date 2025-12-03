f283b80a0f76fed51dd587b97e3394df3fac1eaf
Jason Yoder <jason.yoder2@bsu.edu>
2025-12-03 14:32:53 -0500

    Increases test coverage on storage_service_test.dart

--- test/middleware/storage_service_test.dart
+++ test/middleware/storage_service_test.dart
@@ -6,40 +6,25 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPreferencesMock extends Mock implements SharedPreferences {}
 
 void main() {
-  late SharedPreferencesMock sharedPrefsMock;
   late StorageService storageService;
 
-  setUp(() {
-    sharedPrefsMock = SharedPreferencesMock();
+  setUp(() async {
+    SharedPreferences.setMockInitialValues({});
     storageService = StorageService();
-    storageService.storageInstance = sharedPrefsMock;
+    await storageService.init();
   });
 
   test("setValue saves to SharedPreferences", () async {
-    when(() => sharedPrefsMock.setString(any(), any()))
-        .thenAnswer((_) async => true);
-
     await storageService.setValue("testKey", "testValue");
-
-    verify(() => sharedPrefsMock.setString("testKey", "testValue")).called(1);
-  });
-
-  test("getValue returns stored value", () async {
-    when(() => sharedPrefsMock.getString("testKey"))
-        .thenReturn("testValue");
-
     final result = await storageService.getValue("testKey");
 
     expect(result, "testValue");
   });
 
-  test('Tes that initializer works', () async {
-    // Provide fake initial values
-    SharedPreferences.setMockInitialValues({});
+  test("getValue returns stored value", () async {
+    await storageService.setValue("testKey", "testValue");
+    final result = await storageService.getValue("testKey");
 
-    final service = StorageService();
-    await service.init();
-
-    expect(service.storageInstance, isNotNull);
+    expect(result, "testValue");
   });
 }
