29e97ab58e18f49ea57c27d8cc98dd505b8333db
Jason Yoder <jason.yoder2@bsu.edu>
2025-12-03 10:49:27 -0500

    Rename Middleware to middleware for flutter standard

--- test/middleware/storage_service_test.dart
+++ test/middleware/storage_service_test.dart
@@ -1,4 +1,4 @@
-import 'package:clean_stream_laundry_app/Middleware/storage_service.dart';
+import 'package:clean_stream_laundry_app/middleware/storage_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:shared_preferences/shared_preferences.dart';
