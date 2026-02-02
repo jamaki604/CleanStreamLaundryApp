b6496cf08df6e328aa7df02f7262ff9af91b14ab
Jason Yoder <jason.yoder2@bsu.edu>
2026-02-02 12:51:09 -0500

    Fixes stripe configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    implementation("com.google.android.material:material:1.11.0")
 }
 
 flutter {
