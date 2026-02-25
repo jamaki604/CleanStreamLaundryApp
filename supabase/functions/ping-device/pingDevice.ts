e0b51ab30ffe5bf34c7ddf413b83b954cdfb5a27
Jason Yoder <jason.yoder2@bsu.edu>
2026-02-24 19:54:50 -0500

    Removed plaintext key

--- supabase/functions/ping-device/pingDevice.ts
+++ supabase/functions/ping-device/pingDevice.ts
@@ -1,8 +1,10 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-const SUPABASE_URL = "https://dnuuhupoxjtwqzaqylvb.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRudXVodXBveGp0d3F6YXF5bHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3MDIzOTMsImV4cCI6MjA3NTI3ODM5M30.W6CvYhQlRcsKV6NJLU99aAI4-woHpYZ63hZbD4WeTW4";
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+
+const supabaseUrl = Deno.env.get("SUPABASE_URL");
+const anonKey = Deno.env.get("SUPABASE_ANON_KEY");
+const supabase = createClient(supabaseUrl, anonKey);
 async function getMachineStatusFromSupabase(deviceId) {
   try {
     const { data, error } = await supabase.from('Machines').select('Status').eq('id', deviceId).single();
