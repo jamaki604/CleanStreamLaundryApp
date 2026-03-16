2c795f1c1c5be0db0939295152c0cb8835023670
Joshua Miller <tyfsak61@outlook.com>
2026-03-16 13:59:50 -0400

    Fixed start_machine_tests
    Fixed start_machine_tests to align with new code

--- test/pages/start_machine_page_test.dart
+++ test/pages/start_machine_page_test.dart
@@ -1,3 +1,5 @@
+import 'package:clean_stream_laundry_app/logic/services/auth_service.dart';
+import 'package:clean_stream_laundry_app/logic/services/profile_service.dart';
 import 'package:clean_stream_laundry_app/logic/viewmodels/loyalty_view_model.dart';
 import 'package:clean_stream_laundry_app/pages/start_machine_page.dart';
 import 'package:clean_stream_laundry_app/services/kisi/door_unlocker.dart';
@@ -10,20 +12,34 @@ import 'package:mocktail/mocktail.dart';
 
 class MockDoorUnlocker extends Mock implements DoorUnlocker {}
 class MockLoyaltyViewModel extends Mock implements LoyaltyViewModel {}
+class MockProfileService extends Mock implements ProfileService {}
+class MockAuthService extends Mock implements AuthService {}
 
 void main() {
-  late MockLoyaltyViewModel mockViewModel;
   late MockDoorUnlocker mockUnlocker;
+  late MockLoyaltyViewModel mockViewModel;
+  late MockProfileService mockProfileService;
+  late MockAuthService mockAuthService;
 
   setUp(() {
     mockUnlocker = MockDoorUnlocker();
     mockViewModel = MockLoyaltyViewModel();
+    mockProfileService = MockProfileService();
+    mockAuthService = MockAuthService();
 
     final getIt = GetIt.instance;
-    if (getIt.isRegistered<LoyaltyViewModel>()) {
-      getIt.unregister<LoyaltyViewModel>();
-    }
+
+    if (getIt.isRegistered<LoyaltyViewModel>()) getIt.unregister<LoyaltyViewModel>();
+    if (getIt.isRegistered<ProfileService>()) getIt.unregister<ProfileService>();
+    if (getIt.isRegistered<AuthService>()) getIt.unregister<AuthService>();
+
     getIt.registerSingleton<LoyaltyViewModel>(mockViewModel);
+    getIt.registerSingleton<ProfileService>(mockProfileService);
+    getIt.registerSingleton<AuthService>(mockAuthService);
+
+    when(() => mockAuthService.getCurrentUserId).thenReturn("user123");
+    when(() => mockProfileService.getUserBalanceById("user123"))
+        .thenAnswer((_) async => {"balance": 50.0});
   });
 
   Widget createStartPageTestApp(DoorUnlocker unlocker) {
@@ -33,39 +49,34 @@ void main() {
           path: '/',
           builder: (_, __) => StartPage(doorUnlocker: unlocker),
         ),
-      ],
-    );
-
-    return MaterialApp.router(routerConfig: router);
-  }
-
-  Widget createRouterTestApp() {
-    final router = GoRouter(
-      routes: [
-        GoRoute(
-          path: '/',
-          builder: (_, __) => StartPage(),
-        ),
         GoRoute(
           path: '/scanner',
           builder: (_, __) => const Scaffold(body: Text('Scanner Page')),
         ),
       ],
     );
-
     return MaterialApp.router(routerConfig: router);
   }
 
   Future<void> scrollToUnlockButton(WidgetTester tester) async {
-    await tester.drag(find.byType(SingleChildScrollView), const Offset(0, -500));
-    await tester.pumpAndSettle();
+    final scrollViewFinder = find.descendant(
+      of: find.byType(StartPage),
+      matching: find.byType(SingleChildScrollView),
+    );
+    expect(scrollViewFinder, findsOneWidget);
+
+    await tester.drag(scrollViewFinder, const Offset(0, -500));
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 100));
   }
 
   testWidgets('Tapping QR button navigates to /scanner', (tester) async {
     when(() => mockViewModel.userBalance).thenReturn(50.0);
 
-    await tester.pumpWidget(createRouterTestApp());
+    await tester.pumpWidget(createStartPageTestApp(mockUnlocker));
     await tester.pumpAndSettle();
+
+    await scrollToUnlockButton(tester);
 
     final qrButton = find.widgetWithText(QRButton, "Scan QR code");
     expect(qrButton, findsOneWidget);
@@ -78,16 +89,13 @@ void main() {
 
   testWidgets('Unlock button shows searching dialog', (tester) async {
     when(() => mockViewModel.userBalance).thenReturn(50.0);
-
-    when(() => mockUnlocker.unlockNearestDoor())
-        .thenAnswer((_) async {
+    when(() => mockUnlocker.unlockNearestDoor()).thenAnswer((_) async {
       await Future.delayed(const Duration(milliseconds: 50));
       return true;
     });
 
     await tester.pumpWidget(createStartPageTestApp(mockUnlocker));
     await tester.pumpAndSettle();
-
     await scrollToUnlockButton(tester);
 
     await tester.tap(find.text("Unlock Door"));
@@ -95,8 +103,7 @@ void main() {
 
     expect(find.byType(Dialog), findsOneWidget);
     expect(find.textContaining("Finding Nearby Doors"), findsOneWidget);
-
-    await tester.pump(const Duration(milliseconds: 100));
+    await tester.pumpAndSettle();
   });
 
   testWidgets('Successful unlock closes searching dialog and shows success dialog',
@@ -133,17 +140,23 @@ void main() {
   });
 
   testWidgets('shows low balance dialog when balance is below 20', (tester) async {
-    when(() => mockViewModel.userBalance).thenReturn(19.99);
+    const String testUid = "user123";
+    when(() => mockAuthService.getCurrentUserId).thenReturn(testUid);
+    when(() => mockProfileService.getUserBalanceById(testUid))
+        .thenAnswer((_) async => {"balance": 15.0});
 
     await tester.pumpWidget(createStartPageTestApp(mockUnlocker));
+    await tester.pump();
     await tester.pumpAndSettle();
-
     await scrollToUnlockButton(tester);
 
-    await tester.tap(find.text('Unlock Door'));
+    final unlockButton = find.text('Unlock Door');
+    expect(unlockButton, findsOneWidget);
+    await tester.tap(unlockButton);
     await tester.pumpAndSettle();
 
-    expect(find.text('You need at least \$20 to unlock a door'), findsOneWidget);
+    expect(find.text('Low Balance'), findsOneWidget);
+    expect(find.textContaining('at least 20.00'), findsOneWidget);
     verifyNever(() => mockUnlocker.unlockNearestDoor());
   });
 
